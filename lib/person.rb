9e3a463cdf4a8ed115c51649e21f59384dcd981d
github username <github email address>
2020-04-22 16:40:25 +0100

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+#Class definition for a Person class
+class Person
+
+    #Define 2 instance methods that both print statements
+    def talk
+        puts "Hello World!"
+    end
+    
+    def walk
+        puts "The Person is walking"
+    end
+    
+    end
